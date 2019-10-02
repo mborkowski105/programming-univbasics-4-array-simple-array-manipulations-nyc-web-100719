e3e54292f28e7be1d347e49a3d88653ab920673d
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-02 18:19:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,3 +13,18 @@ end
 def pop_with_args(arr)
   return arr.pop(2)
 end
+
+def using_shift(arr)
+  return arr.shift
+end
+
+def shift_with_args(arr)
+  return arr.shift(2)
+end
+
+def using_concat(arr1, arr2)
+  arr1.concat(arr2)
+end
+
+def using_insert(arr, element)
+  arr.insert
